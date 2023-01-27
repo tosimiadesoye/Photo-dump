a559c0766b0aef699d3803d53f2e1d63712588e4
Tosimi <tosimiadesoye111@gmail.com>
2023-01-27 17:20:52 +0000

    feat: (main): add 2d animation

--- src/main.ts
+++ src/main.ts
@@ -1,52 +1,91 @@
 import "./style.css";
+import { data } from "./data";
 
 const randomNum = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 const shiftGrid = () => {
-  const canvasParent: any = document.getElementById("canvas-parent");
-  canvasParent.style.transform = `translate(${randomNum(0, 100)}px, ${randomNum(
-    0,
-    150
-  )}px)`;
-};
-
-const getRandomColor = () => {
-  let r = (255 * Math.random()) | 0,
-    g = (255 * Math.random()) | 0,
-    b = (255 * Math.random()) | 0;
-  return `rgb(${r}, ${g}, ${b} )`;
-};
-
-const rectangle = (context: {
-  [x: string]: any;
-  beginPath: () => void;
-  rect: (arg0: number, arg1: number, arg2: number, arg3: number) => void;
-  stroke: () => void;
-}) => {
-  for (let x = 0; x < 5; x++) {
-    for (let y = 0; y < 5; y++) {
-      context?.beginPath();
-      context.fillStyle = getRandomColor();
-      context?.fillRect(x * randomNum(0, 300), y * randomNum(0, 200), 100, 150);
-      context?.stroke();
-    }
+  const imgParents: NodeList | any =
+    document.querySelectorAll("#div-img__parents");
+  const img: NodeList | any = document.querySelectorAll("#img-items");
+  for (let i = 0; i < imgParents.length; i++) {
+    imgParents[i].style.transform = `translate(${randomNum(
+      0,
+      100
+    )}px, ${randomNum(0, 150)}px)`;
+    img[i].style.width = `${randomNum(70, 200)}px`;
   }
 };
 
-const canvases = () => {
-  let canvas: HTMLCanvasElement | any = document.getElementById("myCanvas");
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+const scaleImg = () => {
+  const img: NodeList | any = document.querySelectorAll("#img-items");
+  const zoomIn = () => {
+    for (let i = 0; i < img.length; i++) {
+      img[i].onpointerover = () => {
+        img[i].style.transiton = `transform 1279s ease-in-out .51s`;
+        img[i].style.transform = `scale(${randomNum(1.2, 1.5)})`;
+      };
+    }
+    return img;
+  };
 
-  let context: CanvasRenderingContext2D | any = canvas.getContext("2d");
-
-  rectangle(context);
-
-  return context;
+  const zoomOut = () => {
+    for (let i = 0; i < img.length; i++) {
+      img[i].onpointerout = () => {
+        img[i].style.transiton = `transform 0.1s ease-in-out`;
+        img[i].style.transform = `scale(1)`;
+      };
+    }
+  };
+  zoomIn();
+  zoomOut();
 };
 
+const addText = () => {
+  const div: NodeListOf<Element> | any =
+    document.querySelectorAll("#div-img__parents");
+
+  for (let i = 0; i < data.length; i++) {
+    const p = document.createElement("p");
+    p.innerText = data[i].name;
+    p.style.position = "absolute";
+    p.style.display = "none";
+    p.style.zIndex = "999";
+
+    div[i]?.appendChild(p);
+
+    div[i].onpointerover = (e: PointerEvent) => {
+      p.style.left = e.x + "px";
+      p.style.top = e.y + "px";
+      p.style.display = "block";
+    };
+
+    div[i].onpointerout = () => {
+      p.style.display = "none";
+    };
+  }
+
+  return div;
+};
+
+const getImages = () => {
+  const viewport = document.getElementById("viewport");
+  for (let i = 0; i < data.length; i++) {
+    const div = document.createElement("div");
+    div.className = "div-img__parents";
+    div.id = "div-img__parents";
+    const img = document.createElement("img");
+    img.className = "img-items ";
+    img.id = "img-items";
+    img.src = data[i].image;
+    img.alt = data[i].name;
+    div.appendChild(img);
+    viewport?.appendChild(div);
+  }
+
+  return viewport;
+};
 const transformViewport = (x: number, y: number) => {
   const speed = 0.5;
   const num = 2 - 1;
@@ -65,8 +104,10 @@ const transformViewport = (x: number, y: number) => {
   };
 };
 const getContainer = () => {
-  canvases();
+  getImages();
   shiftGrid();
+  scaleImg();
+  addText();
   const viewport: any = document.getElementById("viewport");
 
   window.addEventListener(
