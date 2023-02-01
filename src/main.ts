09fec23b9dd771a896dc4c25494e1aa0934899e6
Tosimi <tosimiadesoye111@gmail.com>
2023-02-01 16:36:21 +0000

    feat(index): add laptop only

--- src/main.ts
+++ src/main.ts
@@ -9,149 +9,161 @@ let index: string | null;
 const text: HTMLParagraphElement = document.createElement("h3");
 const shuffledData = shuffleArray(data);
 
-const shiftGrid = () => {
-  const imgParents: NodeListOf<HTMLElement> =
-    document.querySelectorAll("#div-img__parents");
+if (window.innerWidth < 500) {
+  let app = document.getElementById("app") as HTMLElement;
+  app.style.display = "block";
+  app.style.margin ="auto"
+  app.style.textAlign = "center"
 
-  for (let i = 0; i < imgParents.length; i++) {
-    imgParents[i].style.transform = `translate(${randomNum(
-      0,
-      100
-    )}px, ${randomNum(0, 150)}px)`;
-    imgParents[i].style.width = `${randomNum(50, 100)}px`;
-    imgParents[i].style.height = `${randomNum(50, 100)}px`;
-  }
-};
+} else {
+  const shiftGrid = () => {
+    const imgParents: NodeListOf<HTMLElement> =
+      document.querySelectorAll("#div-img__parents");
 
-const scaleImg = () => {
-  const img: NodeListOf<HTMLElement> = document.querySelectorAll("#img-items");
-  const zoomIn = () => {
-    for (let i = 0; i < img.length; i++) {
-      img[i].onpointerover = () => {
-        img[i].style.width = "300px";
-        img[i].style.height = "300px";
-      };
+    for (let i = 0; i < imgParents.length; i++) {
+      imgParents[i].style.transform = `translate(${randomNum(
+        0,
+        100
+      )}px, ${randomNum(0, 150)}px)`;
+      imgParents[i].style.width = `${randomNum(50, 100)}px`;
+      imgParents[i].style.height = `${randomNum(50, 100)}px`;
     }
+  };
+
+  const scaleImg = () => {
+    const img: NodeListOf<HTMLElement> =
+      document.querySelectorAll("#img-items");
+    const zoomIn = () => {
+      for (let i = 0; i < img.length; i++) {
+        img[i].onpointerover = () => {
+          img[i].style.width = "300px";
+          img[i].style.height = "300px";
+        };
+      }
+      return img;
+    };
+
+    const zoomOut = () => {
+      for (let i = 0; i < img.length; i++) {
+        img[i].onpointerout = () => {
+          img[i].style.width = `${randomNum(70, 200)}px`;
+          img[i].style.height = `${randomNum(70, 200)}px`;
+        };
+      }
+    };
+    zoomIn();
+    zoomOut();
+  };
+
+  const fadeImg = (img: HTMLImageElement) => {
+    let opacity = 0;
+
+    const fadeIn = setInterval(() => {
+      opacity += 0.3;
+
+      img.style.opacity = `${opacity}`;
+
+      if (opacity >= 1) clearInterval(fadeIn);
+    }, 100);
+  };
+
+  const addText = () => {
+    const parent = document.getElementById("parent-text__hover");
+
+    for (let i = 0; i < shuffledData.length; i++) {
+      const div = document.createElement("div");
+
+      if (index === data[i].id) {
+        text.innerText = data[i].name!;
+        div?.appendChild(text);
+        parent?.appendChild(div);
+        text.setAttribute(
+          "style",
+          `--left: ${pointerX}px; --top:${pointerY}px`
+        );
+
+        if (index === null) text.style.display = "none";
+      }
+    }
+
+    return parent;
+  };
+
+  const pointerEventsImg = (img: HTMLImageElement, id: string) => {
+    img.onpointermove = () => {
+      addText();
+      index = id;
+      text.className = "p-text__hover";
+    };
+
+    img.onpointerenter = (e: PointerEvent) => {
+      pointerX = e.x;
+      pointerY = e.y;
+    };
+
     return img;
   };
 
-  const zoomOut = () => {
-    for (let i = 0; i < img.length; i++) {
-      img[i].onpointerout = () => {
-        img[i].style.width = `${randomNum(70, 200)}px`;
-        img[i].style.height = `${randomNum(70, 200)}px`;
-      };
+  const getImages = () => {
+    const viewport = document.getElementById("viewport");
+
+    for (let i = 0; i < shuffledData.length; i++) {
+      const div = document.createElement("div");
+
+      div.id = "div-img__parents";
+
+      const img: HTMLImageElement = document.createElement("img");
+      img.className = "img-items";
+      img.id = "img-items";
+
+      img.src = data[i].image!;
+      img.alt = data[i].name!;
+
+      fadeImg(img);
+
+      div.appendChild(img);
+      viewport?.appendChild(div);
+      pointerEventsImg(img, data[i].id!);
     }
-  };
-  zoomIn();
-  zoomOut();
-};
 
-const fadeImg = (img: HTMLImageElement) => {
-  let opacity = 0;
-
-  const fadeIn = setInterval(() => {
-    opacity += 0.1;
-
-    img.style.opacity = `${opacity}`;
-
-    if (opacity >= 1) clearInterval(fadeIn);
-  }, 100);
-};
-
-const addText = () => {
-  const parent = document.getElementById("parent-text__hover");
-
-  for (let i = 0; i < shuffledData.length; i++) {
-    const div = document.createElement("div");
-
-    if (index === data[i].id) {
-      text.innerText = data[i].name!;
-      div?.appendChild(text);
-      parent?.appendChild(div);
-      text.setAttribute("style", `--left: ${pointerX}px; --top:${pointerY}px`);
-
-      if (index === null) text.style.display = "none";
-    }
-  }
-
-  return parent;
-};
-
-const pointerEventsImg = (img: HTMLImageElement, id: string) => {
-  img.onpointermove = () => {
-    addText();
-    index = id;
-    text.className = "p-text__hover";
+    return viewport;
   };
 
-  img.onpointerenter = (e: PointerEvent) => {
-    pointerX = e.x;
-    pointerY = e.y;
+  const transformViewport = (x: number, y: number) => {
+    const speed = 0.5;
+    const num = 2 - 1;
+
+    const normaliseX = (x / window.innerWidth) * num;
+    const percentageX = normaliseX * 100;
+    const translateX = percentageX * speed;
+
+    const normaliseY = (y / window.innerHeight) * num;
+    const percentageY = normaliseY * 100;
+    const translateY = percentageY * speed;
+
+    return {
+      x: translateX,
+      y: translateY,
+    };
   };
 
-  return img;
-};
+  const getContainer = () => {
+    getImages();
+    shiftGrid();
+    scaleImg();
 
-const getImages = () => {
-  const viewport = document.getElementById("viewport");
+    const viewport = document.getElementById("viewport");
 
-  for (let i = 0; i < shuffledData.length; i++) {
-    const div = document.createElement("div");
+    window.addEventListener(
+      "pointermove",
+      (e: PointerEvent) => {
+        const { x, y } = transformViewport(e.x, e.y);
 
-    div.id = "div-img__parents";
-
-    const img: HTMLImageElement = document.createElement("img");
-    img.className = "img-items";
-    img.id = "img-items";
-
-    img.src = data[i].image!;
-    img.alt = data[i].name!;
-
-    fadeImg(img);
-
-    div.appendChild(img);
-    viewport?.appendChild(div);
-    pointerEventsImg(img, data[i].id!);
-  }
-
-  return viewport;
-};
-
-const transformViewport = (x: number, y: number) => {
-  const speed = 0.5;
-  const num = 2 - 1;
-
-  const normaliseX = (x / window.innerWidth) * num;
-  const percentageX = normaliseX * 100;
-  const translateX = percentageX * speed;
-
-  const normaliseY = (y / window.innerHeight) * num;
-  const percentageY = normaliseY * 100;
-  const translateY = percentageY * speed;
-
-  return {
-    x: translateX,
-    y: translateY,
+        if (typeof viewport !== "undefined")
+          viewport!.style.transform = `translate(${-x * -2}px, ${-y * -2}px)`;
+      },
+      false
+    );
   };
-};
-
-const getContainer = () => {
-  getImages();
-  shiftGrid();
-  scaleImg();
-
-  const viewport = document.getElementById("viewport");
-
-  window.addEventListener(
-    "pointermove",
-    (e: PointerEvent) => {
-      const { x, y } = transformViewport(e.x, e.y);
-
-      if (typeof viewport !== "undefined")
-        viewport!.style.transform = `translate(${-x * -2}px, ${-y * -2}px)`;
-    },
-    false
-  );
-};
-getContainer();
+  getContainer();
+}
